ff078d780a5cc77f250282be919145baf9a93f81
NathanMorasse <2133133@etu.cchic.ca>
2023-10-16 12:44:54 -0400

    affichage grille oneway

--- HourGlassUnlimited/HourGlassUnlimited/ViewModels/VM.cs
+++ HourGlassUnlimited/HourGlassUnlimited/ViewModels/VM.cs
@@ -5,15 +5,13 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DevExpress.Mvvm;
 using System.Reflection.Metadata;
 
 namespace HourGlassUnlimited.ViewModels
 {
-    public abstract class VM : INotifyPropertyChanged, ISupportParameter
+    public abstract class VM : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler? PropertyChanged;
-        public object? Parameter { get; set; }
         public virtual void ChangeValue(string property)
         {
             if (this.PropertyChanged != null)
