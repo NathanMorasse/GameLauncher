a6d3beeb9fb02be5605768ba5d435e5cbb2a6123
Jesus <2133752@etu.cchic.ca>
2023-10-18 12:57:54 -0400

    Update de la navigation

--- HourGlassUnlimited/HourGlassUnlimited/Games/Sudoku/ViewModels/GameMenuVM.cs
+++ HourGlassUnlimited/HourGlassUnlimited/Games/Sudoku/ViewModels/GameMenuVM.cs
@@ -15,15 +15,17 @@ namespace HourGlassUnlimited.Games.Sudoku.ViewModels
 {
     public class GameMenuVM : VM 
     {     
-        public ICommand Normal { get; set; }
+        public ICommand Launch_Classic { get; set; }
+        public ICommand Launch_Daily { get; set; }
 
         public GameMenuVM()
         {
-            this.Normal = new CommandLink(Normal_Execute, Normal_CanExecute);
+            this.Launch_Classic = new CommandLink(Launch_Classic_Execute, Launch_Classic_CanExecute);
+            this.Launch_Daily = new CommandLink(Launch_Daily_Execute, Launch_Daily_CanExecute);
         }
 
-        private bool Normal_CanExecute(object parameter) { return true; }
-        private async void Normal_Execute(object parameter) 
+        private bool Launch_Classic_CanExecute(object parameter) { return true; }
+        private async void Launch_Classic_Execute(object parameter) 
         {
             SudokuGame game = new SudokuGame();
             game.IsDaily = true;
@@ -31,6 +33,12 @@ namespace HourGlassUnlimited.Games.Sudoku.ViewModels
             game.GameBoard = await dal.SudokuFact.GenerateBoard("easy");
             SudokuNavigator.GamePage.SetGame(game);
             SudokuNavigator.GamePageView(); 
+        }
+
+        private bool Launch_Daily_CanExecute(object parameter) { return true; }
+        private async void Launch_Daily_Execute(object parameter)
+        {
+            SudokuNavigator.GamePageView();
         }
     }
 }
--- HourGlassUnlimited/HourGlassUnlimited/Games/Sudoku/ViewModels/SudokuWindowVM.cs
+++ HourGlassUnlimited/HourGlassUnlimited/Games/Sudoku/ViewModels/SudokuWindowVM.cs
@@ -1,13 +1,47 @@
-﻿using HourGlassUnlimited.ViewModels;
+﻿using HourGlassUnlimited.Games.Sudoku.Tools;
+using HourGlassUnlimited.Tools;
+using HourGlassUnlimited.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace HourGlassUnlimited.Games.Sudoku.ViewModels
 {
     public class SudokuWindowVM : VM
     {
+        public ICommand Exit_To_Launcher { get; set; }
+        public ICommand Back_To_Menu { get; set; }
+
+        public SudokuWindowVM() : base()
+        {
+            this.Exit_To_Launcher = new CommandLink(Exit_To_Launcher_Execute, Exit_To_Launcher_CanExecute);
+            this.Back_To_Menu = new CommandLink(Back_To_Menu_Execute, Back_To_Menu_CanExecute);
+        }
+
+        private bool Exit_To_Launcher_CanExecute(object parameter) { return true; }
+        private async void Exit_To_Launcher_Execute(object parameter)
+        {
+            if (Navigator.MainWindow.IsVisible)
+            {
+                Navigator.MainWindow.WindowState = System.Windows.WindowState.Maximized;
+            }
+            else
+            {
+                Navigator.MainWindow.Show();
+            }
+
+            SudokuNavigator.Close();
+        }
+
+        private bool Back_To_Menu_CanExecute(object parameter) { return true; }
+        private async void Back_To_Menu_Execute(object parameter)
+        {
+            SudokuNavigator.GameMenuView();
+        }
+
+
     }
 }
