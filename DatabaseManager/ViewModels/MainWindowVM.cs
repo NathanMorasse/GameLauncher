9eda55812dbdc402250fcd8e020b9bb6d42a99dc
Jasus <2133752@etu.cchic.ca>
2023-11-24 09:55:12 -0500

    Finis le DAL des room

--- DatabaseManager/ViewModels/MainWindowVM.cs
+++ DatabaseManager/ViewModels/MainWindowVM.cs
@@ -12,15 +12,22 @@ namespace DatabaseManager.ViewModels
     public class MainWindowVM : ViewModelTemplate
     {
         public ICommand DepartmentList {  get; set; }
+        public ICommand RoomList {  get; set; }
 
         public MainWindowVM() 
         {
             this.DepartmentList = new CommandLink(DepartmentList_Execute, Dummy_CanExecute);
+            this.RoomList = new CommandLink(RoomList_Execute, Dummy_CanExecute);
         }
 
         private void DepartmentList_Execute(object parameter)
         {
             Navigator.DepartmentList();
+        }
+
+        private void RoomList_Execute(object parameter)
+        {
+            Navigator.RoomList();
         }
     }
 }
